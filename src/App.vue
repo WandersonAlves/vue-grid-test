b636c434aaace416785fa7536e5e2b73143a62e4
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2017-04-03 13:31:18 -0300

    Some bootstrap things on main app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="text-center">
-                <p>{{msg}}</p>
+<div>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div>
+                <p class="navbar-text">{{msg}}</p>
             </div>
         </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            <form id="search">
-                Search <input name="query" v-model="searchQuery">
-            </form>
+    </nav>
+    <div class="jumbotron">
+        <div class="container">
+            <div class="page-header">
+                <h1>Hello mysterious company! <br><small>Hope you like it!</small></h1>
+            </div>
+            <p>So far, we have concluded these requirements:</p>
+            <div class="input-group">
+                <input type="checkbox" checked disabled aria-label=""> Sort the contracts from the table columns
+            </div>
+            <div class="input-group">
+                <input type="checkbox" disabled aria-label=""> Add / Remove Table Columns
+            </div>
+            <div class="input-group">
+                <input type="checkbox" checked disabled aria-label=""> Find a line by text (from any column)
+            </div>
+            <div class="input-group">
+                <input type="checkbox" checked disabled aria-label=""> Edit record
+            </div>
+            <div class="input-group">
+                <input type="checkbox" disabled aria-label=""> You should use a datepicker for dates
+            </div>
+            <div class="input-group">
+                <input type="checkbox" disabled aria-label=""> There should be a validation of the inserted data
+            </div>
             <br>
+            <p>This application was made with Vue.js, and this was my first time trying it!</p>
+            <!-- /input-group -->
         </div>
     </div>
-    <div class="row">
-        <grid-component :columns="gridColumns" :gridData="gridData" :searchQuery="searchQuery"></grid-component>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <form id="search">
+                    <label class="form-control-label">Search</label>
+                    <input class="form-control" name="query" v-model="searchQuery">
+                </form>
+                <br>
+            </div>
+        </div>
+        <div class="row">
+            <grid-component :columns="gridColumns" :gridData="gridData" :searchQuery="searchQuery"></grid-component>
+        </div>
     </div>
 </div>
 </template>
@@ -45,7 +76,6 @@ export default {
             this.$http.get('https://api.myjson.com/bins/n25id').then(response => {
                 this.gridData = response.data.contracts;
             }, response => {
-                // error callback
             });
         }
     },
@@ -54,3 +84,8 @@ export default {
     }
 }
 </script>
+<style>
+body {
+    padding-top: 50px;
+}
+</style>
